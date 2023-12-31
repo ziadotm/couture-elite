586e07ef0d4e88a5af620f8ec6283eb95d99d053
otm <ziadotm.12@gmail.com>
2023-12-31 14:59:47 +0100

    first commit

---
+++ admin.php
@@ -0,0 +1,107 @@
+<?php
+
+class Admin{
+
+public $id;
+public $firstname;
+public $lastname;
+public $email;
+public $password;
+
+
+
+public static $errorMsg = "";
+
+public static $successMsg="";
+
+
+public function __construct($firstname2,$lastname,$email,$password){
+
+    //initialize the attributs of the class with the parameters, and hash the password
+    $this->firstname = $firstname2;
+    $this->lastname = $lastname;
+    $this->email = $email;
+    $this->password = password_hash($password,PASSWORD_DEFAULT);
+   
+
+}
+
+public function insertAdmin($tableName,$conn){
+
+//insert a admin in the database, and give a message to $successMsg and $errorMsg
+$sql = "INSERT INTO $tableName (firstname, lastname, email,password)
+VALUES ('$this->firstname', '$this->lastname', '$this->email','$this->password')";
+if (mysqli_query($conn, $sql)) {
+self::$successMsg= "New record created successfully";
+
+} else {
+    self::$errorMsg ="Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+
+
+}
+
+public static function  selectAllAdmin($tableName,$conn){
+
+//select all the client from database, and inset the rows results in an array $data[]
+$sql = "SELECT id, firstname, lastname,email,idCity FROM $tableName ";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        $data=[];
+        while($row = mysqli_fetch_assoc($result)) {
+        
+            $data[]=$row;
+        }
+        return $data;
+    }
+
+}
+
+static function selectAdminById($tableName,$conn,$id){
+    //select a client by id, and return the row result
+    $sql = "SELECT firstname, lastname,email FROM $tableName  WHERE id='$id'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    $row = mysqli_fetch_assoc($result);
+    
+    }
+    return $row;
+}
+
+static function updateAdmin($Admin,$tableName,$conn,$id){
+    //update a client of $id, with the values of $client in parameter
+    //and send the user to read.php
+    $sql = "UPDATE $tableName SET lastname='$Admin->lastname',firstname='$Admin->firstname',email='$Admin->email' WHERE id='$id'";
+        if (mysqli_query($conn, $sql)) {
+        self::$successMsg= "New record updated successfully";
+header("Location:read.php");
+        } else {
+            self::$errorMsg= "Error updating record: " . mysqli_error($conn);
+        }
+
+
+}
+
+static function deleteAdmin($tableName,$conn,$id){
+    //delet a client by his id, and send the user to read.php
+    $sql = "DELETE FROM $tableName WHERE id='$id'";
+
+if (mysqli_query($conn, $sql)) {
+    self::$successMsg= "Record deleted successfully";
+    header("Location:read.php");
+} else {
+    self::$errorMsg= "Error deleting record: " . mysqli_error($conn);
+}
+
+  
+    }
+
+
+  
+
+}
+
+?>
---
+++ loginadmin.php
@@ -0,0 +1,140 @@
+<?php
+//include connection file
+include('connection.php');
+   
+
+//create in instance of class Connection
+$connection = new Connection();
+
+
+//call the selectDatabase method
+$connection->selectDatabase('vetement');
+$emailValue = "";
+$lnameValue = "";
+$fnameValue = "";
+$passwordValue = "";
+$errorMesage = "";
+$successMesage = "";
+if(isset($_POST["submit"])){
+
+    $emailValue = $_POST["email"];
+    $lnameValue = $_POST["lastName"];
+    $fnameValue = $_POST["firstName"];
+    $passwordValue = $_POST["password"];
+   
+
+    if(empty($emailValue) || empty($fnameValue) || empty($lnameValue) || empty($passwordValue)){
+
+            $errorMesage = "all fileds must be filed out!";
+
+    }else if(strlen($passwordValue) < 8 ){
+        $errorMesage = "password must contains at least 8 char";
+    }else if(preg_match("/[A-Z]+/", $passwordValue)==0){
+        $errorMesage = "password must contains  at least one capital letter!";}
+    else if(preg_match("/\w+(@emsi-edu.com){1}$/",$emailValue)==1 ){
+    $emailerror="Please enter a valid Email";
+        }
+     else{
+       
+    
+    //include the client file
+    include('admin.php');
+
+    //create new instance of client class with the values of the inputs
+    $admin = new Admin($fnameValue,$lnameValue,$emailValue,$passwordValue);
+
+//call the insertClient method
+$admin->insertAdmin('admin',$connection->conn);
+
+//give the $successMesage the value of the static $successMsg of the class
+$successMesage = Admin::$successMsg;
+
+//give the $errorMesage the value of the static $errorMsg of the class
+$errorMesage = Admin::$errorMsg;
+
+$emailValue = "";
+$lnameValue = "";
+$fnameValue = "";   
+      
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container my-5 ">
+
+        <h2>SIGN UP</h2>
+
+    <?php
+
+    if(!empty($errorMesage)){
+  echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+  <strong>$errorMesage</strong>
+  <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'>
+  </button>
+  </div>";
+    }
+       ?>
+
+        <br>
+        <form method="post">
+            <div class="row mb-3">
+                    <label class="col-form-label col-sm-1" for="fname">First Name:</label>
+                    <div class="col-sm-6">
+                        <input value="<?php echo $fnameValue ?>" class="form-control" type="text" id="fname" name="firstName">
+                    </div>
+            </div>
+            <div class="row mb-3">
+                    <label class="col-form-label col-sm-1" for="lname">Last Name:</label>
+                    <div class="col-sm-6">
+                        <input  value="<?php echo $lnameValue ?>" class="form-control" type="text" id="lname" name="lastName">
+                    </div>
+            </div>
+            <div class="row mb-3 ">
+                    <label class="col-form-label col-sm-1" for="email">Email:</label>
+                    <div class="col-sm-6">
+                        <input value=" <?php echo $emailValue ?>" class="form-control" type="email" id="email" name="email">
+                    </div>
+          
+            <div class="row mb-3 ">
+                    <label class="col-form-label col-sm-1" for="password">Password:</label>
+                    <div class="col-sm-6">
+                        <input  class="form-control" type="password" id="password" name="password" >
+                    </div>
+            </div>
+
+            <?php
+            if(!empty($successMesage)){
+echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+<strong>$successMesage</strong>
+<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'>
+</button>
+</div>";
+            }
+  ?>  
+      
+
+            <div class="row mb-3">
+                    <div class="offset-sm-1 col-sm-3 d-grid">
+                        <button name="submit" type="submit" class=" btn btn-primary">Signup</button>
+                    </div>
+                    <div class="col-sm-1 col-sm-3 d-grid">
+                        <a class="btn btn-outline-primary" href="frontadmin.php">Login</a>
+                    </div>
+            </div>
+        </form>
+
+    </div>
+
+</body>
+</html>
